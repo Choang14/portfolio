e5194406fdab1999b82f3817d862ca4ca79f525e
Choang14 <Cindy.hoang914@gmail.com>
2023-05-03 17:53:23 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import { Button } from 'react-bootstrap';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <Button variant="success">Boot Strap Button</Button>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "error",
+    element: <div>There is an error</div>,
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
